1d04eb98d1934328aad66dff48b3c4764918cb56
xShiro2 <daryllejumalon16@gmail.com>
2023-04-19 10:12:28 +0800

    feat: add questions and answers

--- questions.js
+++ questions.js
@@ -1,14 +1,80 @@
 const questions = [
     {
-        question: "Blasdasldlas",
-        answer: "test",
+        question: "Tic-tac-toe is played on a grid with how many squares?",
+        answer: "9",
         choices: [
-            "balbaba",
-            "blalba",
-            "test",
-            "asdasda"
+            "1",
+            "3",
+            "5",
+            "9"
         ]
-    }
+    },
+
+    {
+        question: "Players win in tic-tac-toe when they succeed in placing three of their marks in a row in what manner?",
+        answer: "any of the above",
+        choices: [
+            "vertically",
+            "horizontally",
+            "diagonally",
+            "any of the above"
+        ]
+    },
+
+    {
+        question: "If you live in England, what might you call tic-tac-toe?",
+        answer: "noughts and crosses",
+        choices: [
+            "lawn darts",
+            "noughts and crosses",
+            "beans and honey",
+            "chess"
+        ]
+    },
+
+    {
+        question: "How many players are needed to play tic-tac-toe?",
+        answer: "2",
+        choices: [
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        question: "What was the name of the early varation of tic-tac-toe played in the Roman Empire?",
+        answer: "terni lapilli",
+        choices: [
+            "da paura",
+            "terni lapilli",
+            "scialla",
+            "anvedi"
+        ]
+    },
+
+    {
+        question: "Year that Sandy Douglas developed OXO (or Noughts and Crosses) for the EDSAC computer",
+        answer: "1952",
+        choices: [
+            "1910",
+            "1999",
+            "1952",
+            "1971"
+        ]
+    },
+
+    {
+        question: "Studies shows that when X makes the first move, the game outcomes is/are: ",
+        answer: "All of the above",
+        choices: [
+            "91 distinct positions are won by (X)",
+            "44 distinct positions are won by (O)",
+            "3 distinct positions are drawn",
+            "All of the above"
+        ]
+    },
 ]
 
 export { questions }
